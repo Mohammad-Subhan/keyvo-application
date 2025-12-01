9ee22657d0e5d156634da9d4ed81139ee7cc26b2
Mohammad-Subhan <subhankhalid185@gmail.com>
2025-12-02 01:37:32 +0500

    Added basic code

---
+++ lib/theme/app_theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const Color backgroundColor = Color(0xFF020A1A);
+  static const Color primaryColor = Color(0xFFDB5447);
+  static const Color textColor = Color(0xFFDEDFE1);
+}
